2c7d7e1ec2a1a21c0d36ea7ea1d8166c4d9ae708
spde3289 <spde3289@naver.com>
2022-02-07 10:00:24 +0900

    ss

--- html/09.VOGUE_PJ/002.site/js/common.js
+++ html/09.VOGUE_PJ/002.site/js/common.js
@@ -5,6 +5,9 @@ window.addEventListener("DOMContentLoaded",
     () => {
         console.log("로딩완료!");
 
+        /// 부드러운 스크롤 호출!(제이쿼리 아님!)
+        startSS();
+
         /*********************************** 
                     로드구역 전역변수
         ***********************************/
@@ -30,6 +33,23 @@ window.addEventListener("DOMContentLoaded",
             console.log("페이지위치값",i,"번째:",scPos[i]);
         } ///////// for문 ////////////////////////
 
+        /****************************************** 
+            함수명: scAction
+            기능: 스크롤 위치값이 설정범위에 들어가면
+                해당 순번의 요소가 등장한다!
+        ******************************************/
+       const scAction = seq => { // seq 순번
+
+            // console.log("순번:",seq);
+
+            if (scTop > scPos[seq]-winH && // 시작위치
+                scTop < scPos[seq]) { // 끝위치
+                scAct[seq].classList.add("on");
+            } ///////// if ////////////////////
+
+       }; /////////// scAction 함수 ////////////////////
+       /////////////////////////////////////////////////
+
 
         // 변경대상 /////////////////////
         // 상단영역 - #top
@@ -37,8 +57,14 @@ window.addEventListener("DOMContentLoaded",
         // 위로가기 버튼 - .tbtn
         let tbtn = document.querySelector(".tbtn");
 
+        /////// 위로가기버튼 클릭시 맨위로 가기! ///////
+        /// 부드러운 스크롤의 위치변수 pos에 0값을 주면 위로이동함
+        tbtn.onclick = () => {pos=0; return false;};
+        // return false -> a요소의 기본이동기능을 못하게 막음!
 
-        ///// 윈도우 스크롤 이벤트 함수 //////
+        ////////////////////////////////////////////
+        ///////// 윈도우 스크롤 이벤트 함수 /////////
+        ////////////////////////////////////////////
         // 스크롤 이벤트 : scroll
         // window객체에 스크롤이벤트를 걸어준다!(전체스크롤)
         window.addEventListener("scroll", () => {
@@ -55,12 +81,10 @@ window.addEventListener("DOMContentLoaded",
                 3. document.scrollingElement.scrollTop
                 4. document.documentElement.scrollTop
                 5. document.querySelector("html").scrollTop
-
                 참고) 가로스크롤일 경우
                     scrollY -> scrollX
                     scrollTop -> scrollLeft
                     로 바꿔서 위와 동일함!
-
             **************************************************/
 
             // console.log("스위:",scTop);
@@ -98,15 +122,33 @@ window.addEventListener("DOMContentLoaded",
             /////// 등장액션 클래스 주기 /////
             /////////////////////////////////
 
-            if (scTop > scPos[0]-winH && scTop < scPos[0]) {
-                scAct[0].classList.add("on");
-            }
-            else if (scTop > scPos[1]-winH && scTop < scPos[1]) {
-                scAct[1].classList.add("on");
-            }
-            else if (scTop > scPos[2]-winH && scTop < scPos[2]) {
-                scAct[2].classList.add("on");
-            }
+            // scAct 변수에 담긴 등장액션할 요소의 개수만큼
+            // scPos 배열변수에 위치값이 셋팅되어 있다!
+            // 이 배열을 이용하여 forEach() 메서드로
+            // 위치체크를 하여 클래스"on"을 넣어주는 함수
+            // scAction() 함수를 순번과 함께 호출해 준다!!!
+
+            scPos.forEach((val,idx)=>scAction(idx));
+            // 배열객체.forEach((배열값,순번)=>{});
+
+            /* 
+            배열객체.forEach(function(item,index,arr){})
+            
+            - forEach는 함수내부에 3가지 값을
+            전달한다!
+            1. item : 배열의 값
+            2. index : 배열의 순번(0부터~)
+            3. arr : 배열객체자신!
+            -> 전달변수의 숫자와 순서가 중요함!
+            -> 전달변수명은 다르게 쓸 수 있음!
+            _________________________
+            다르게 표현해 보자!
+                        
+            배열객체.forEach((배열값)=>{})
+            배열객체.forEach((배열값,순번)=>{})
+            배열객체.forEach((배열값,순번,배열자신)=>{})
+                    
+          */
 
             /*********************************************** 
                 [ 클래스 관리객체 : classList ]
