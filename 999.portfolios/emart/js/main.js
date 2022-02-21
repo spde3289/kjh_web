9a04e76b8d8aca9bd3be1e2c46a2648bd9157ce7
spde3289 <spde3289@naver.com>
2022-02-22 00:34:34 +0900

    ss

--- 999.portfolios/emart/js/main.js
+++ 999.portfolios/emart/js/main.js
@@ -1,12 +1,23 @@
 window.addEventListener("load",() => {
     let anis = document.getElementById("ani");
     let stop = document.getElementById("stop");
+    let start = document.getElementById("start");
 
     function ani() {
-        anis.classList.remove("ani");
+        document.getElementById("ani").style.animationPlayState="paused";
+        document.getElementById("stop").style.display="none";
+        document.getElementById("start").style.display="block";
         console.log("stop");
     }
-    
+
+    function reani() {
+        document.getElementById("ani").style.animationPlayState="running";
+        document.getElementById("start").style.display="none";
+        document.getElementById("stop").style.display="block";
+        console.log("start");
+    }
+
     stop.addEventListener('click', ani);
+    start.addEventListener('click', reani);
 });
 
